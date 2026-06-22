e8adf7c85ebd41cbd98981c9c226136686599b8b
Alek Åström <alek.astrom@gmail.com>
2026-06-22 21:24:11 +0200

    Pin Apple native SPM revisions for 6.5.4

--- ios/agora_rtc_engine/Package.swift
+++ ios/agora_rtc_engine/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "agora-rtc-engine", targets: ["agora_rtc_engine"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", .upToNextMajor(from: "4.6.2"))
+        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", revision: "db11601e5cc10e74d4f03157c063711156838e75")
     ],
     targets: [
         .target(
